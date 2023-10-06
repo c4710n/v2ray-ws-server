93cad9c8bb9d3c498b813fd65ec2b0d2dbb6bec9
c4710n <c4710n@users.noreply.github.com>
2023-10-06 19:21:11 +0800

    fix: fix bad overlay

--- flake.nix
+++ flake.nix
@@ -6,25 +6,25 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+    (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
         bash-tpl = pkgs.callPackage ./pkgs/bash-tpl.nix { };
         v2ray-ws-server = pkgs.callPackage ./pkgs/v2ray-ws-server.nix { inherit bash-tpl; };
-
-        devShell = pkgs.mkShell { buildInputs = [ bash-tpl pkgs.v2ray ]; };
       in
       {
-        devShells.default = devShell;
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ bash-tpl pkgs.v2ray ];
+        };
 
         packages.default = v2ray-ws-server;
         packages.v2ray-ws-server = v2ray-ws-server;
-
-        overlays.default = final: prev: rec {
-          bash-tpl = prev.callPackage ./pkgs/bash-tpl.nix { };
-          v2ray-ws-server = prev.callPackage ./pkgs/v2ray-ws-server.nix { inherit bash-tpl; };
-        };
       }
-    );
+    )) // {
+      overlays.default = final: prev: rec {
+        bash-tpl = prev.callPackage ./pkgs/bash-tpl.nix { };
+        v2ray-ws-server = prev.callPackage ./pkgs/v2ray-ws-server.nix { inherit bash-tpl; };
+      };
+    };
 }
