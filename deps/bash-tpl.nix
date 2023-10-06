c8e265fc79a0f20d38f09df6cf1e95be034f9902
c4710n <c4710n@users.noreply.github.com>
2023-10-06 11:13:59 +0800

    deps: create derivation for bash-tpl

---
+++ deps/bash-tpl.nix
@@ -0,0 +1,31 @@
+pkgs:
+let
+  inherit (pkgs) lib stdenv fetchFromGitHub;
+
+  pname = "bash-tpl";
+  version = "0.7.1";
+in
+stdenv.mkDerivation {
+  inherit pname version;
+
+  src = fetchFromGitHub {
+    owner = "TekWizely";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-psUgUj2dieSINTapblMeDYZcD+GAFRl+9wfZnieMffQ=";
+  };
+
+  installPhase = ''
+    install -Dm755 -t $out/bin bash-tpl
+
+    substituteInPlace "$out/bin/bash-tpl" \
+      --replace "/usr/bin/env bash" "${stdenv.shell}"
+  '';
+
+  meta = with lib; {
+    description = "A smart, lightweight shell script templating engine, written in Bash";
+    homepage = "https://github.com/TekWizely/bash-tpl";
+    license = licenses.mit;
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}
